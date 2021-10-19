afc786c656101dd193481b3935e35d1871c315a4
Konstantin <konstantin.retunsky@gmail.com>
2021-10-19 17:52:27 +0500

    add dialog show

--- android/app/src/main/java/com/testnativemodule/CalendarModule.java
+++ android/app/src/main/java/com/testnativemodule/CalendarModule.java
@@ -1,8 +1,12 @@
 package com.testnativemodule;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -26,6 +30,29 @@ public class CalendarModule extends ReactContextBaseJavaModule {
 	@ReactMethod
 	public void createCalendarEvent(String name, String location) {
 		Log.d("CalendarModule", "Create event called with name: " + name + " and location: " + location);
-	}
 
+		AppCompatActivity activity = (AppCompatActivity) getCurrentActivity();
+		if (activity == null) return;
+
+		AlertDialog alertDialog = new AlertDialog.Builder(activity).create();
+//
+//		// Указываем Title
+		alertDialog.setTitle("Информационое сообщение");
+
+		// Указываем текст сообщение
+		alertDialog.setMessage("Welcome to devcolibri.com");
+
+		// Обработчик на нажатие OK
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// Код который выполнится после закрытия окна
+				Toast.makeText(activity, "Вы нажали OK", Toast.LENGTH_SHORT).show();
+			}
+		});
+//
+//		// показываем Alert
+		alertDialog.show();
+
+//		MainActivity
+	}
 }
