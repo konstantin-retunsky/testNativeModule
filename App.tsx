4f57a495ac91c6ab8c0bc6069b3d1b4c87cfb553
Konstantin <konstantin.retunsky@gmail.com>
2021-10-19 16:47:38 +0500

    add native module

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import CalendarModule from './src/CalendarModule';
 
 interface Props {
-  
+
 }
 
+const NewModuleButton = () => {
+
+  const onPress = () => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+    console.log('We will invoke the native module here!');
+  };
+
+  return <Button
+    title="Click to invoke your native module!"
+    color="#841584"
+    onPress={onPress}
+  />;
+};
+
 const App = (props: Props) => {
+
   return (
     <View>
+      <NewModuleButton />
       <Text>
         hello eeboi
       </Text>
