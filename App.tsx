b712ecad7d4fe0f59fd0788a81eab90ba3eafc2a
Konstantin <konstantin.retunsky@gmail.com>
2021-10-19 15:29:49 +0500

    init

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+interface Props {
+  
+}
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <Text>
+        hello eeboi
+      </Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
